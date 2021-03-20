ad8a55e7e3e4e49f97d49bc01a8af87abaea09e4
galinha-chan <galinhapento@gmail.com>
2021-03-20 01:41:13 -0300

    First commit

---
+++ numowner.js
@@ -0,0 +1,12 @@
+const ownerNumber = ["5521982660837@s.whatsapp.net"]
+const OriginalOwner = '5521982660837'
+const vcard = 'BEGIN:VCARD\n' 
+            + 'VERSION:3.0\n' 
+            + 'FN:Meu criador^~^\n' 
+            + 'ORG:Ian;\n' 
+            + 'TEL;type=CELL;type=VOICE;waid=5521982660837:+55 21 98266-0837\n' 
+            + 'END:VCARD'
+
+exports.vcard = vcard
+exports.OriginalOwner = OriginalOwner
+exports.ownerNumber = ownerNumber
---
+++ src/welcomtext.js
@@ -0,0 +1,9 @@
+const welcome = (number, groupname) => {
+    return `𝐎𝐥𝐚 @${number}. 𝐒𝐞𝐣𝐚 𝐛𝐞𝐦-𝐯𝐢𝐧𝐝𝐨 𝐚𝐨 𝐠𝐫𝐮𝐩𝐨 ${groupname}`
+}
+exports.welcome = welcome
+
+const bye = (number) => {
+    return `Adeus @${number}. Não volta por favor ✓`
+}
+exports.bye = bye
